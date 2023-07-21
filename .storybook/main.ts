63712ecf79bc44a3dc892def9ee6b3b6e958fe58
Apologizer <71948843+Shaa-Gizer@users.noreply.github.com>
2023-07-21 18:31:14 +0300

    added snapshot test for AddItemForm

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -7,6 +7,20 @@ const config: StorybookConfig = {
     "@storybook/preset-create-react-app",
     "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
+    {
+      name: '@storybook/addon-storysource',
+      options: {
+        rule: {
+          test: [/\.stories\.tsx?$/],
+        },
+        loaderOptions: {
+          prettierConfig: {
+            printWidth: 80, singleQuote: false,
+            options: {parser: 'typescript'}
+          },
+        },
+      },
+    }
   ],
   framework: {
     name: "@storybook/react-webpack5",
